8dca0598301ebb9379169bd3d08c71c7490029d5
Ethan Fung <EthanEFung@gmail.com>
2022-10-24 20:25:18 -0700

    mvp

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
 require('dotenv').config()
+const upload = multer()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+const viewHandler = (req, res) => 
   res.sendFile(process.cwd() + '/views/index.html');
-});
 
+const uploadHandler = (req, res) => {
+  console.log(req.file);
 
+  if (!req.file) {
+    res.send({ error: 'upload a file to analyze' })
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+}
+
+app.get('/', viewHandler);
+
+app.post('/api/fileanalyse', upload.single('upfile'), uploadHandler); 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+
+const listen = (err) => err
+  ? console.log("Failure to start", err)
+  : console.log('Your app is listening on port ' + port)
+
+app.listen(port, listen);
